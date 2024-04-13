4fbf937e35a32adc63bc9d16200cfa2ff795ff00
dennyneelamkavil <dennynj4748@gmail.com>
2024-04-13 11:01:46 +0530

    form added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 function Contact() {
   return (
-    <div>Contact Page</div>
+    <Container>
+      <Row>
+        <Col md={6} className='d-flex justify-content-center'>
+          <div>
+            <form action="">
+              <input type="text" name='fullname' placeholder='Please enter fullname ' />
+              <input type="email" name='email' placeholder='Please enter email ' />
+              <input type="number" name='phone' placeholder='Please enter phone number ' />
+              <textarea type="text" rows={10} name='message' placeholder='Please enter message '>
+                </textarea>
+            </form>
+
+          </div>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
